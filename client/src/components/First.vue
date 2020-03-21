f145619c6b5b2206d85c548e54dd4e8d624dc752
Pavel Los <flombik2013@yandex.by>
2020-03-21 13:39:59 +0300

    Initial commit

---
+++ client/src/components/First.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <router-link to="/books">Books</router-link>
+    |
+    <router-link to="/authors">Authors</router-link>
+    |
+    <router-link to="/publishers">Publishers</router-link>
+    |
+    <router-link to="/genres">Genres</router-link>
+  </div>
+</template>
---
+++ client/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import First from '../components/First.vue';
+import Books from '../components/Books.vue';
+import Authors from '../components/Authors.vue';
+import Publishers from '../components/Publishers.vue';
+import Genres from '../components/Genres.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'First',
+    component: First,
+  },
+  {
+    path: '/books',
+    name: 'Books',
+    component: Books,
+  },
+  {
+    path: '/authors',
+    name: 'Authors',
+    component: Authors,
+  },
+  {
+    path: '/publishers',
+    name: 'Publishers',
+    component: Publishers,
+  },
+  {
+    path: '/genres',
+    name: 'Genres',
+    component: Genres,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
